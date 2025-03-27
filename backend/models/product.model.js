a546a2fea95e36369a11bc6c2584848019ed880d
20230093Buddila <139336336+20230093Buddila@users.noreply.github.com>
2025-03-27 08:23:35 +0530

    Github Actions Test

--- backend/models/product.model.js
+++ backend/models/product.model.js
@@ -1,6 +1,5 @@
 import mongoose from "mongoose";
 
-
 const productSchema = mongoose.Schema({
     name:{
         type: String,
--- frontend/src/store/product.js
+++ frontend/src/store/product.js
@@ -36,6 +36,7 @@ export const useProductStore = create((set) => ({
 		set((state) => ({ products: state.products.filter((product) => product._id !== pid) }));
 		return { success: true, message: data.message };
 	},
+	
     updateProduct: async (pid, updatedProduct) => {
 		const res = await fetch(`/api/products/${pid}`, {
 			method: "PUT",
